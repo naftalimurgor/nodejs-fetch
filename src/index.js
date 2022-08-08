3eaa2a307bb9ebee1ed084c13b6d46345b138ab2
Naftali Murgor <murgornaftali@gmail.com>
2022-08-08 13:32:01 +0300

    refactor: wrap async-await in a `try-catch`

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 (async() => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-  const todos = await res.json()
-  console.log({todos})
+  try {    
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const todos = await res.json()
+    console.log({todos})
+  } catch (error) {
+    console.error(`Faile to Fetched:`, error)
+  }
 })()
